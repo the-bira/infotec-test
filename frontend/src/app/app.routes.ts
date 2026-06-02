2aa7169a501fa2d7ad203e9c6e52335090e2095a
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2026-06-02 16:36:01 -0300

    feat(frontend): configure protected routing for vehicles management pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,25 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { VehiclesList } from './pages/vehicles-list/vehicles-list';
+import { VehicleForm } from './pages/vehicle-form/vehicle-form';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: Login }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { 
+    path: 'vehicles', 
+    component: VehiclesList, 
+    canActivate: [authGuard] 
+  },
+  { 
+    path: 'vehicles/new', 
+    component: VehicleForm, 
+    canActivate: [authGuard] 
+  },
+  { 
+    path: 'vehicles/edit/:id', 
+    component: VehicleForm, 
+    canActivate: [authGuard] 
+  }
 ];
